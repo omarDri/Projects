87e1d84db540e1899391ae25b74156a44e67a709
aortmann <alvinortmann@yahoo.de>
2022-12-31 11:01:20 +0100

    fixed norm errors

--- Projects/Basics/libft/ft_strjoin.c
+++ Projects/Basics/libft/ft_strjoin.c
@@ -18,12 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	size_t	j;
 
+	i = 0;
+	j = 0;
 	str = (char *)malloc(sizeof(*s1)
 			* (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (s1[i])
 	{
 		str[j++] = s1[i];
